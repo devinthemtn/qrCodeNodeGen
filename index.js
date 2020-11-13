c93d7069a99550bb9acdbb27a2f5d0457ba04429
Thomas Koefod <tom@nativeteachingaids.com>
2020-11-13 14:34:57 -0700

    packages added

---
+++ index.js
@@ -0,0 +1,7 @@
+const PORT = 3000;
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const fs = require("fs");
+const QRCode = require("qrcode");
